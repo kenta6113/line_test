c299728b38493ce7709dbe0a16c99d19538183fe
KentaNakagawa <n.kenta.30308@gmail.com>
2021-10-28 20:17:37 +0900

    練習中

---
+++ ohayo_bot.py
@@ -0,0 +1,54 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+
+# %%
+get_ipython().system('pip install line-bot-sdk ')
+
+
+# %%
+import json
+
+
+# %%
+file = open('info.json','r')
+info = json.load(file)
+
+
+# %%
+info['CHANNEL_ACCESS_TOKEN']
+
+
+# %%
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+
+# %%
+CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN )
+
+
+# %%
+def main():
+    USER_ID = info["USER_ID"]
+    messages = TextSendMessage(text="おはよ〜　\n 朝だよ！起きてね♡")
+    line_bot_api.push_message(USER_ID,messages=messages)
+    
+    
+main()
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
